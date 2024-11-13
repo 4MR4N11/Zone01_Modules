a147f316b5a5c2df7fd87a79e6557ec78e94152a
kelamrani <4mr4n11kh4l1d@gmail.com>
2024-11-13 15:33:27 +0100

    default, -B -P -O flags are Done

--- wget/internal/downloader/http.go
+++ wget/internal/downloader/http.go
@@ -3,8 +3,11 @@ package downloader
 import (
 	"fmt"
 	"io"
+	"math"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"wget/internal/utils"
@@ -15,72 +18,103 @@ func HttpDownloader(args utils.Args) {
 	for i, url := range args.Url {
 		if args.UrlFile[i] == "" {
 			args.UrlFile[i] = "index.html"
-			// url = strings.TrimRight(url, "/") + "/" + args.UrlFile[i]
 		}
-		fmt.Println(url)
+		output := ""
 		startTime := time.Now()
-		fmt.Println("start at " + startTime.Format("2006-01-02 15:04:05"))
-		fmt.Print("sending request, awaiting response... ")
+		output += fmt.Sprintln("start at " + startTime.Format("2006-01-02 15:04:05"))
+		output += fmt.Sprint("sending request, awaiting response... ")
 		resp, err := client.Get(url)
 		if err != nil {
 			fmt.Println("Error downloading", err)
 			os.Exit(1)
 		}
 		defer resp.Body.Close()
-		fmt.Println("status " + resp.Status)
-
+		output += fmt.Sprintln("status " + resp.Status)
+		if resp.StatusCode != 200 {
+			os.Exit(1)
+		}
 		contentSize := resp.ContentLength
 		if contentSize == -1 {
-			fmt.Println("Warning: Unknown content size, progress will not be accurate")
+			output += fmt.Sprintln("Warning: Unknown content size, progress will not be accurate")
+			contentSize = 0
+		} else if contentSize != -1 {
+			output += fmt.Sprintf("content size: %d [~%s]\n", contentSize, formatBytestoMb(contentSize))
 		}
-		if contentSize != -1 {
-			fmt.Printf("content size: %d [~%dMB]\n", contentSize, contentSize/1024)
+		if len(args.Output) > 0 {
+			args.UrlFile[i] = args.Output
 		}
-
+		if len(args.Path) > 0 {
+			if strings.HasPrefix(args.Path, "~") {
+				homeDir, err := os.UserHomeDir()
+				if err != nil {
+					fmt.Println("Error", err)
+					os.Exit(1)
+				}
+				args.Path = filepath.Join(homeDir, args.Path[1:])
+			}
+			args.UrlFile[i] = args.Path + "/" + args.UrlFile[i]
+		} else {
+			args.UrlFile[i] = "./" + args.UrlFile[i]
+		}
 		outputFile, _ := os.Create(args.UrlFile[i])
-		fmt.Printf("saving file to: ./%s\n", args.UrlFile[i])
+		output += fmt.Sprintf("saving file to: %s\n", args.UrlFile[i])
 		defer outputFile.Close()
-
-		// bar := progressbar.NewOptions(int(contentSize),
-		// 	progressbar.OptionShowBytes(true),
-		// 	progressbar.OptionSetWidth(100),
-		// 	progressbar.OptionSetTheme(progressbar.Theme{
-		// 		Saucer:        "=",
-		// 		SaucerHead:    ">",
-		// 		SaucerPadding: " ",
-		// 		BarStart:      "[",
-		// 		BarEnd:        "]",
-		// 	}))
-
 		var downloadedBytes int64
 		buffer := make([]byte, 8*1024)
+		if !args.Bdownload {
+			fmt.Print(output)
+			output = ""
+		}
 		for {
 			n, err := resp.Body.Read(buffer)
 			if err == io.EOF {
 				break
 			}
 			if err != nil {
-				fmt.Println("Error during download:", err)
+				output += fmt.Sprintln("Error during download:", err)
 				os.Exit(1)
 			}
 			outputFile.Write(buffer[:n])
 			downloadedBytes += int64(n)
 			elapsedTime := time.Since(startTime)
 			downloadSpeed := float64(downloadedBytes) / 1024 / 1024 / elapsedTime.Seconds()
-			progressPercentage := float64(downloadedBytes) / float64(contentSize) * 100
-			elapsedTimeStr := fmt.Sprintf("%.0fs", elapsedTime.Seconds())
-			fmt.Printf("\r%v / %v [%s] %.2f%% %.2f MiB/s %v",
-				formatBytes(downloadedBytes),
-				formatBytes(contentSize),
-				progressbar(progressPercentage),
-				progressPercentage,
-				downloadSpeed,
-				elapsedTimeStr,
-			)
-
+			progressPercentage := 0.0
+			remainingBytes := contentSize - downloadedBytes
+			remainingTimeStr := ""
+			if downloadSpeed > 0 {
+				if contentSize == 0 {
+					remainingTimeStr = "unknown"
+				} else {
+					remainingTime := int(remainingBytes / int64(downloadSpeed*1024*1024))
+					remainingTimeStr = fmt.Sprintf("%ds", remainingTime)
+				}
+			} else {
+				remainingTimeStr = "unknown"
+			}
+			if contentSize == 0 {
+				progressPercentage = 0.0
+			} else {
+				progressPercentage = float64(downloadedBytes) / float64(contentSize) * 100
+			}
+			if !args.Bdownload {
+				fmt.Printf("\r%v / %v [%s] %.2f%% %.2f MiB/s %s   ",
+					formatBytes(downloadedBytes),
+					formatBytes(contentSize),
+					progressbar(progressPercentage),
+					progressPercentage,
+					downloadSpeed,
+					remainingTimeStr,
+				)
+			}
 		}
-		fmt.Printf("\nDownloaded [%s]\n", args.Url[i])
-		fmt.Printf("finished at [%s]\n", time.Now().Format("2006-01-02 15:04:05"))
+		output += fmt.Sprintf("\nDownloaded [%s]\n", args.Url[i])
+		output += fmt.Sprintf("finished at [%s]\n", time.Now().Format("2006-01-02 15:04:05"))
+		if !args.Bdownload {
+			fmt.Print(output)
+			output = ""
+		} else {
+			utils.PrintOutput(args, output)
+		}
 	}
 }
 
@@ -104,5 +138,17 @@ func formatBytes(bytes int64) string {
 		return fmt.Sprintf("%.2f MiB", float64(bytes)/1024/1024)
 	} else {
 		return fmt.Sprintf("%.2f GiB", float64(bytes)/1024/1024/1024)
+	}
+}
+
+func formatBytestoMb(bytes int64) string {
+	if bytes < 1000 {
+		return fmt.Sprintf("%d B", bytes)
+	} else if bytes < 1000*1000 {
+		return fmt.Sprintf("%.2f KB", math.Round(float64(bytes)/1000))
+	} else if bytes < 1000*1000*1000 {
+		return fmt.Sprintf("%.2f MB", math.Round(float64(bytes)/1000/1000))
+	} else {
+		return fmt.Sprintf("%.2f GB", math.Round(float64(bytes)/1000/1000/1000))
 	}
 }
--- wget/internal/utils/outputFormat.go
+++ wget/internal/utils/outputFormat.go
@@ -2,36 +2,15 @@ package utils
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 	"os"
-
-	"github.com/schollz/progressbar/v3"
 )
 
-func PrintOutput(args Args, resp *http.Response, file *os.File) {
-	fmt.Println(resp.ContentLength)
-	bar := progressbar.NewOptions(int(resp.ContentLength),
-		progressbar.OptionEnableColorCodes(true),
-		progressbar.OptionShowBytes(true),
-		progressbar.OptionSetWidth(100),
-		progressbar.OptionSetTheme(progressbar.Theme{
-			Saucer:        "[green]=[reset]",
-			SaucerHead:    "[green]>[reset]",
-			SaucerPadding: " ",
-			BarStart:      "[",
-			BarEnd:        "]",
-		}),
-		progressbar.OptionUseIECUnits(true),
-		progressbar.OptionShowTotalBytes(true))
-	if int(resp.ContentLength) == -1 {
-		for i := 0; i < 1000; i++ {
-			bar.Add(1)
-		}
+func PrintOutput(args Args, output string) {
+	if args.Bdownload {
+		logFile, _ := os.Create("wget-log")
+		logFile.WriteString(output)
+		fmt.Println("Output will be written to \"wget-log\".")
 	} else {
-		for i := 0; i < int(resp.ContentLength); i++ {
-			bar.Add(1)
-		}
+		fmt.Print(output)
 	}
-	io.Copy(io.MultiWriter(file, bar), resp.Body)
 }
